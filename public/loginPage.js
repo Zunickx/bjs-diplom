847491fd84c783c4fd85e779a4b7426b640d9d6b
Zunickx <Zunickx@gmail.com>
2023-07-06 22:24:16 +0300

    Change loginFormCallback ver 1.1

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,28 +2,24 @@
 
 const userForm = new UserForm();
 
-class UserForm {
-    constructor() {
-        this.loginFormCallback = function(data) { // данные переданные в форму входа, используем в функции и передаем в ApiConnector для сверки с сервером
-            ApiConnector.login(data, (response) => {
-                if(response.success) {            // где в дальнейшем проверяем ответ на истину, если равно истине, то
-                    location.reloaded();          // перезагружаем страницу
-                } else {
-                    console.error(response.error);// если иначе, то выводит ошибку
-                }
-            })
+userForm.loginFormCallback = function(data) { // данные переданные в форму входа, используем в функции и передаем в ApiConnector для сверки с сервером
+    ApiConnector.login(data, (response) => {
+        if(response.success) {            // где в дальнейшем проверяем ответ на истину, если равно истине, то
+            location.reloaded();          // перезагружаем страницу
+        } else {
+            userForm.setLoginErrorMessage(response.error);// если иначе, то выводит ошибку
         }
+    })
+}
 
-        this.registerFormCallback = function(data) {
-            ApiConnector.login(data, (response) => {
-                if(response.success) {
-                    location.reload();
-                } else {
-                    console.error(response.error);
-                }
-            })
+userForm.registerFormCallback = function(data) {
+    ApiConnector.login(data, (response) => {
+        if(response.success) {
+            location.reload();
+        } else {
+            console.error(response.error);
         }
-    }
+    })
 }
 
 
